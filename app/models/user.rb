1799af2c5bcb7108701842e17fdaa0e36d294647
bLngSt <75664742+bLngSt@users.noreply.github.com>
2021-01-08 16:55:46 +0900

    hoge修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nick_name,           presence: true
+  validates :first_name,          presence: true
+  validates :last_name,           presence: true
+  validates :katakana_first_name, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  validates :katakana_Last_name,  presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  validates :birthday,            presence: true
 end
+katakana_first_name:katakana_Last_name,
