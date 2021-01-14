b543d236ff315c7bdff42d5b359fa2a31e4246fc
bLngSt <75664742+bLngSt@users.noreply.github.com>
2021-01-14 15:17:43 +0900

    item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,26 @@
 class Item < ApplicationRecord
-  with_options presence: true do
-    validates :name
-    validates :explanation
-    validates :category_id
-    validates :state_id
-    validates :burden_id
-    validates :area_id
-    validates :day_id
-    validates :price
-    validates :user
-  end
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :burden
+  belongs_to :area
+  belongs_to :day
 
   belongs_to :user
+  has_one_attached :image
+  
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :explanation
+    validates :price, :numericality => { with: /\A[0-9]+\z/ ,:only_interger => true, :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999}
+    validates :user
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :state_id
+      validates :burden_id
+      validates :area_id
+      validates :day_id
+    end
+  end
 end
