054f02ac080ece08b6f65edfd53b222c66ba4124
bLngSt <75664742+bLngSt@users.noreply.github.com>
2021-01-22 11:22:05 +0900

    環境変数化

--- app/controllers/user_details_controller.rb
+++ app/controllers/user_details_controller.rb
@@ -29,7 +29,7 @@ class UserDetailsController < ApplicationController
     @item = Item.find(params[:item_id])
   end
   def pay_item
-    Payjp.api_key = "sk_test_a1d97920a066f1983a9a147b"
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
         amount: @item.price,
         card: user_detail_params[:token],
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_9476313f62d43c0d1b4a021a");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
